709d72c49367a7436e0af1103f99eb96560d6f87
Marius Chertes <chertes.marius.d@gmail.com>
2021-01-09 12:56:25 +0200

    Versiunea4

--- src/main/java/ClasaNoua.java
+++ src/main/java/ClasaNoua.java
@@ -1,7 +1,8 @@
 public class ClasaNoua {
     public static void main(String[] args) {
         int i=3;
-        System.out.println(i=1);
+        int a=2;
+        System.out.println(i+a);
 
 }}
 
